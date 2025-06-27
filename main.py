863947978e3ba68eda83de6e9e8e72b9c64ab5b2
angelic-coder19 <angelyuzya7@gmial.com>
2025-06-27 16:25:02 +0200

    Add session information for access control - Import session from flask libary to contain infromation about each user - Configure session to use filesystme and non permanent - Add decorator fuction for access control that will decorate other fuctions - Redirect users who are not logged in to login page

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session
+from functools import wraps
 
+# Configure appliaction
 app = Flask(__name__)
+
+# Configure session to use filesystem instead of signed cookies
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+session(app)
+
+# Login decorator function for access control
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("index.html")
+        return f(*args, **kwargs)
+    return decorated_function
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        mood = request.form.get('mood')
+        print(mood)
+
     return render_template("home.html")
